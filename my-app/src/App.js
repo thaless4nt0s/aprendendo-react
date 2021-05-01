619f79b1338a364682f4c5bf00b373580933a88a
= <=>
2021-05-01 10:08:07 -0300

    Adicionando o arquivo components/welcome.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Greet from "./components/Greet"
+import Welcome from './components/Welcome'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
