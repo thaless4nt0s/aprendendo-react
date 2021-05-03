fa0669e1114f418d2e75798ed3e8fec1b3fb1f9f
= <=>
2021-05-03 16:35:54 -0300

    simplificar o uso do this.state

--- my-app/src/components/Welcome.js
+++ my-app/src/components/Welcome.js
@@ -4,6 +4,7 @@ class Welcome extends Component {
     
     render() {
         const {name, heroName} = this.props
+        //const {state1, state2} = this.state
         return (
             <div>
                 <h1>Name:   {name}</h1>
