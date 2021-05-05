6d44d5cc05be5a2f115dfc1bdb4243180311baca
= <=>
2021-05-04 21:07:29 -0300

    Aprendendo a passar parametro via props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,6 +12,7 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 
 function App() {
@@ -34,7 +35,7 @@ function App() {
 
       {/*<Hello></Hello>*/}
       {/*<Senna></Senna>*/}
-      <EventBind/>
+      <ParentComponent/>
     </div>
   );
 }
