8c9df5aded531908d35b134a8771283beb355d7f
= <=>
2021-05-10 21:15:38 -0300

    Outro exemplo de forms em React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Form from './components/Form';
+import Form2 from './components/Form2';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+      <Form2/>
     </div>
   );
 }
---
+++ my-app/src/components/Form2.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+
+class Form2 extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: "",
+            age: '',
+            characteristics: "",
+            skills: ""
+
+        }
+    }
+
+    handleNameChange = event => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleAgeChange = event => {
+        this.setState({
+            age: event.target.value
+        })
+    }
+
+    handleSkillChange = event => {
+        this.setState({
+            skills: event.target.value
+        })
+    }
+
+    handleSubmitChange = event => {
+        alert(`Name: ${this.state.name}\nAge: ${this.state.age}
+Characteristics: ${this.state.characteristics}\nSkills: ${this.state.skills}`)
+        event.preventDefault()
+    }
+
+    handleCharacteristicsChange = event => {
+        this.setState({
+            characteristics: event.target.value
+        })
+    }
+
+    render() {
+        const { name, age, characteristics, skills } = this.state
+        return (
+            <form onSubmit={this.handleSubmitChange}>
+                <div>
+                    <label>Name</label>
+                    <input
+                        type='text'
+                        value={name}
+                        onChange={this.handleNameChange}
+                    />
+                </div>
+                <div>
+                    <label>Age</label>
+                    <input
+                        type='number'
+                        value={age}
+                        onChange={this.handleAgeChange}
+                    />
+                </div>
+
+                <label>Characteristics</label>
+                <textarea value={characteristics} onChange={this.handleCharacteristicsChange}>
+                </textarea>
+
+                <div>
+                    <label>Skill</label>
+                    <select value={skills} onChange={this.handleSkillChange}>
+                        <option value='react'>React</option>
+                        <option value='bootstrap'>Bootstrap</option>
+                        <option value='php'>php</option>
+                    </select>
+                </div>
+                <input type='color' />
+                <input type="submit" />
+            </form>
+        )
+    }
+}
+
+export default Form2
