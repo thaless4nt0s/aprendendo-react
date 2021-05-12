f56b84d93e342a5cd47bdae9e0fe2befeaba5c58
= <=>
2021-05-12 18:50:15 -0300

    Learning about forwardRef

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import FocusInput from './components/FocusInput';
+import FRParent from './components/FRParent';
 
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+      <FRParent/>
     </div>
   );
 }
