c35b892fb57cf4e8abcb8e535a107a01ff70a78f
= <=>
2021-05-14 14:51:13 -0300

    Another context's form

--- my-app/src/components/ComponentF.js
+++ my-app/src/components/ComponentF.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import ComponentE from './ComponentE'
+import UserContext from './CreateContext'
 
 class ComponentF extends Component {
+    static contextType = UserContext
     render() {
         return (
             <div>
                 <ComponentE/>  
+                <div>Component F context {this.contextType}</div>
             </div>
         )
     }
 }
+
+
 
 export default ComponentF
--- my-app/src/components/CreateContext.js
+++ my-app/src/components/CreateContext.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext('Codevolutuion')
 
 const UserProvider = UserContext.Provider
 
 const UserConsumer = UserContext.Consumer
 
 export {UserProvider, UserConsumer}
+export default UserContext
